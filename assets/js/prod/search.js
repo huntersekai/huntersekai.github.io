982f77539029df2a0b38696103f8236afd67370b
A-1 Fansub <102446742+A-1Fansub@users.noreply.github.com>
2022-06-30 23:49:19 +0700

    tes array sort 4

--- assets/js/prod/search.js
+++ assets/js/prod/search.js
@@ -10,7 +10,7 @@
           appendString += '<li><a href="' + item.url + '" class="h4">' + item.title + '</a></li><br>';
         }
         
-        store.sort();
+        appendString.sort();
         searchResults.innerHTML = appendString;
       } else {
         searchResults.innerHTML = '<li class="h4">Tidak ada.</li>';
