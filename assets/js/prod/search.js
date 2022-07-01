e4279da9a89a80403a69df4b8008553b07d68e27
A-1 Fansub <102446742+A-1Fansub@users.noreply.github.com>
2022-07-01 16:46:17 +0700

    fix

--- assets/js/prod/search.js
+++ assets/js/prod/search.js
@@ -38,20 +38,17 @@
     // a boost of 10 to indicate matches on this field are more important.
     var idx = lunr(function () {
       this.field('title');
-      this.field('date');
-      this.field('id');
+      this.field('id', { boost: 10 });
     });
 
     for (var key in window.store) { // Add the data to lunr
       idx.add({
         'id': key,
-        'title': window.store[key].title,
-        'date': window.store[key].date,
+        'title': window.store[key].title
       });
 
       var results = idx.search(searchTerm); // Get lunr to perform a search
       displaySearchResults(results, window.store); // We'll write this in the next section
-      results.sort();
     }
   }
 })();
