2fc2ff047df9974847d15c32c484f65674d93432
A-1 Fansub <102446742+A-1Fansub@users.noreply.github.com>
2022-07-01 07:54:15 +0700

    hapus boost title

--- assets/js/prod/search.js
+++ assets/js/prod/search.js
@@ -38,7 +38,7 @@
     // a boost of 10 to indicate matches on this field are more important.
     var idx = lunr(function () {
       this.field('id');
-      this.field('title', { boost: 10 });
+      this.field('title');
     });
 
     for (var key in window.store) { // Add the data to lunr
