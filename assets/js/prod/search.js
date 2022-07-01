f11aed443c85b4b2aa56df216833dbbbd8abba55
A-1 Fansub <102446742+A-1Fansub@users.noreply.github.com>
2022-07-01 14:54:54 +0700

    fix

--- assets/js/prod/search.js
+++ assets/js/prod/search.js
@@ -1,34 +1,34 @@
 (function() {
   function displaySearchResults(results, store) {
     var searchResults = document.getElementById('search-results');
-    
-      if (results.length) { // Are there any results?
-        var appendString = '';
-        
-        for (var i = 0; i < results.length; i++) {  // Iterate over the results
-          var item = store[results[i].ref];
-          appendString += '<li><a href="' + item.url + '" class="h4">' + item.title + '</a></li><br>';
-        }
-        
-        searchResults.innerHTML = appendString;
-      } else {
-        searchResults.innerHTML = '<li class="h4">Tidak ada.</li>';
+
+    if (results.length) { // Are there any results?
+      var appendString = '';
+
+      for (var i = 0; i < results.length; i++) {  // Iterate over the results
+        var item = store[results[i].ref];
+        appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
+      }
+
+      searchResults.innerHTML = appendString;
+    } else {
+      searchResults.innerHTML = '<li>No results found</li>';
+    }
+  }
+
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+
+      if (pair[0] === variable) {
+        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
       }
     }
-  
-    function getQueryVariable(variable) {
-      var query = window.location.search.substring(1);
-      var vars = query.split('&');
+  }
 
-      for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split('=');
-
-        if (pair[0] === variable) {
-          return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
-        }
-      }
-    }
-  
   var searchTerm = getQueryVariable('query');
 
   if (searchTerm) {
@@ -37,8 +37,8 @@
     // Initalize lunr with the fields it will be searching on. I've given title
     // a boost of 10 to indicate matches on this field are more important.
     var idx = lunr(function () {
-      this.field('title');
       this.field('id');
+      this.field('title');
     });
 
     for (var key in window.store) { // Add the data to lunr
